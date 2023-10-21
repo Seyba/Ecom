ab315c6374dd86658cfc43ce7e15351b626e1269
Mahamadou <mmohmo@yahoo.com>
2023-10-20 22:39:00 -0400

    email controller added

---
+++ controllers/emailCtr.js
@@ -0,0 +1,10 @@
+const nodeMailer = require('nodemailer')
+const asyncHandler = require('express-async-handler')
+
+const sendMail = asyncHandler(
+    async(data, req, res) => {
+
+    }
+)
+
+module.export = sendMail
