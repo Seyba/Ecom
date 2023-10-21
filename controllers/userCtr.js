179a778fd9e579040334fda5cc9fba73d4ea711d
Mahamadou <mmohmo@yahoo.com>
2023-10-20 22:34:18 -0400

    update controller bug fixed

--- controllers/userCtr.js
+++ controllers/userCtr.js
@@ -202,12 +202,12 @@ const handleTokenRefresh = asyncHandler(
 const updatePassword = asyncHandler(
     async(req, res) => {
         const { _id } = req.user
-        const password = req.body
+        const {password} = req.body
         validateMongoDbId(_id)
         const user = await User.findById(_id)
         if(password) {
             user.password = password
-            const updatedPassword = await User.save()
+            const updatedPassword = await user.save()
             res.json(updatedPassword)
         } else {
             res.json(user)
