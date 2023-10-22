694febbc36c7035e01345a8f0889643046c8d27f
Mahamadou <mmohmo@yahoo.com>
2023-10-22 14:49:04 -0400

    blog category schema added

--- routes/prodcategoryRoutes.js
+++ routes/prodcategoryRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const {isAdmin, authMiddleware} = require('../middlewares/authMiddleware')
 const { createCategory, deleteCategory, getCategories, getCategory, updateCategory } = require('../controllers/prodCategoryCtr')
 
-router.get('/', getCategories)
+router.get('/', authMiddleware, isAdmin, getCategories)
 router.post('/', createCategory)
 router.put('/:id', updateCategory)
 router.get('/:id', getCategory)
