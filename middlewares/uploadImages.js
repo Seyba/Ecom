fc6c09e9dcb78eb05615ada91f9db61bb013f89b
Mahamadou <mmohmo@yahoo.com>
2023-10-23 23:53:09 -0400

    upload picture functionality added

---
+++ middlewares/uploadImages.js
@@ -0,0 +1,32 @@
+const multer = require('multer')
+const sharp = require('sharp')
+const path = require('path')
+
+const multerStorage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, path.join(__dirname, "../public/images"))
+    },
+    filename: function(req, file, cb){
+        const uniqeSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9)
+        cb(null, file.filename + "-" + uniqeSuffix + ".jpeg")
+    }
+})
+
+const multerFilter = (req, file, cb) => {
+    if(file.mimetype.starsWith('image')){
+        cb(null, true)
+    } else {
+        cb({
+            message: "unsupported file formate"
+        }, 
+        false
+        )
+    }
+}
+const uploadPhoto = multer({
+    storage: multerStorage,
+    fileFilter: multerFilter,
+    limits: {fileSize: 2000000}
+})
+
+module.exports = { uploadPhoto}
