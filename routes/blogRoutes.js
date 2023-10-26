eef5e8d6ff5df83a15564b9b84cb864d2af1e866
Mahamadou <mmohmo@yahoo.com>
2023-10-25 21:42:57 -0400

    blog controller and router updated

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const {isAdmin, authMiddleware} = require('../middlewares/authMiddleware')
-const {createBlog, deleteBlog, dislikeBlog, getBlog, getBlogs, likeBlog, updateBlog} = require('../controllers/blogCtr')
+const {createBlog, deleteBlog, dislikeBlog, getBlog, getBlogs, likeBlog, updateBlog, uploadImages} = require('../controllers/blogCtr')
+const { uploadPhoto, blogImgResize} = require('../middlewares/uploadImages')
 
 router.post('/', authMiddleware, isAdmin, createBlog)
+router.put("/upload/:id", authMiddleware, isAdmin, uploadPhoto.array("images", 2), blogImgResize, uploadImages)
 router.put('/likes', authMiddleware, isAdmin, likeBlog)
 router.put('/dislikes', authMiddleware, isAdmin, dislikeBlog)
-
 router.get('/',  getBlogs)
 router.put('/:id', authMiddleware, isAdmin, updateBlog)
 router.get('/:id', authMiddleware, getBlog)
