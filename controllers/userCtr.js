cd2516ec258d33b8556a0742787b95eaea91b9fb
Mahamadou <mmohmo@yahoo.com>
2023-10-28 22:46:03 -0400

    create order controller added

--- controllers/userCtr.js
+++ controllers/userCtr.js
@@ -1,8 +1,10 @@
 const { generateToken } = require('../config/jwttoken')
 const {generateNewToken} = require('../config/refreshToken')
+const uniqid = require('uniqid')
 const User = require('../models/userModel')
 const Product = require('../models/productModel')
 const Coupon = require('../models/couponModel')
+const Order = require('../models/orderModel')
 const Cart = require('../models/cartModel')
 const {validateMongoDbId} = require('../utils/validateMongodbId')
 const asyncHandler = require('express-async-handler')
@@ -454,10 +456,62 @@ const applyCoupon = asyncHandler(
     }
 )
 
+const createOrder = asyncHandler(
+    async(req, res)=> {
+        const { COD, couponApplied } = req.body 
+        const { _id } = req.user
+        validateMongoDbId(_id)
+        
+        try {
+            if (!COD) throw new Error('Create cash order failed')
+            const user = await User.findById(_id)
+            let userCart = await Cart.findOne({orderby: user._id})
+            let finalAmount = 0
+
+            if(couponApplied && userCart.totalAfterDiscount) {
+                finalAmount = userCart.totalAfterDiscount * 100
+            } else {
+                finalAmount = userCart.cartTotal * 100
+            }
+
+            let newOrder = await new Order({
+                products: userCart.products,
+                paymentIntent: {
+                    id: uniqid(), 
+                    method: 'COD', 
+                    amount: finalAmount, 
+                    status: "Cash On Delivery",
+                    created: Date.now(),
+                    currency: "USD",
+                },
+                orderby: user._id,
+                orderStatus: "Cash On Delivery"
+            }).save()
+            let update = userCart.products.map(
+                (item) => {
+                    return {
+                        updateOne: {
+                            filter: {_id: item.product._id},
+                            update: {$inc: {quantity: -item.count, sold: +item.count}}
+                        }
+                    }
+                }
+            )
+
+            const updated = await Product.bulkWrite(update, {})
+            res.json({message: success})
+        } catch (error) {
+            throw new Error(error)
+        }
+        
+    }
+)
+
 module.exports = {
     adminLogin,
     applyCoupon,
     blockUser,
+    createOrder,
     createUser, 
     deleteUser,
     emptyCart,
