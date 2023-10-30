d08f90e8ff9e17017dc8804281ba447c33d28e4a
Mahamadou <mmohmo@yahoo.com>
2023-10-29 23:51:00 -0400

    color router and controller added

--- routes/brandRoutes.js
+++ routes/brandRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const {isAdmin, authMiddleware} = require('../middlewares/authMiddleware')
-const { createColor, deleteColor, getColors, getColor, updateColor } = require('../controllers/colorCtr')
-router.get('/', getColors)
-router.post('/', authMiddleware, isAdmin, createColor)
-router.put('/:id', authMiddleware, isAdmin, updateColor)
-router.get('/:id', getColor)
-router.delete('/:id', authMiddleware, isAdmin, deleteColor)
+const { createBrand, deleteBrand, getBrands, getBrand, updateBrand } = require('../controllers/brandCtr')
+router.get('/', getBrands)
+router.post('/', authMiddleware, isAdmin, createBrand)
+router.put('/:id', authMiddleware, isAdmin, updateBrand)
+router.get('/:id', getBrand)
+router.delete('/:id', authMiddleware, isAdmin, deleteBrand)
 
 module.exports = router
